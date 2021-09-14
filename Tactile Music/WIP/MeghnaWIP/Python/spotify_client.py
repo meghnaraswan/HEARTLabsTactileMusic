4c8b1411065296cef99f1f4de6d8d812e11c7ef8
Meghna Raswan <raswan@chapman.edu>
2021-09-13 23:49:52 -0700

    writing audio features into tracks_log.csv file

--- Tactile Music/WIP/MeghnaWIP/Python/spotify_client.py
+++ Tactile Music/WIP/MeghnaWIP/Python/spotify_client.py
@@ -1,5 +1,6 @@
 import requests
 import base64
+import csv
 # https://docs.python-requests.org/en/master/user/quickstart/
 from conf import config as global_conf
 from local import config as local_conf
@@ -16,7 +17,10 @@ class spotify_client:
                        'Basic '+ encoded_client_id_secret}
         r = requests.post(global_conf.ACCESS_TOKEN_URL, data={'grant_type':'client_credentials'},  headers=headers)
         response_json = r.json()
+        print("")
+        print("Access (Bearer) Token JSON Response:")
         print(response_json)
+        print("")
         return response_json["access_token"]
 
     def get_audio_features(self, access_token, track_id):
@@ -24,8 +28,20 @@ class spotify_client:
                        'Bearer '+ access_token}
         r = requests.get(global_conf.AUDIO_FEATURES_API_URL+track_id,  headers=headers)
         response_json = r.json()
+        print("Audio Feature JSON Response:")
         print(response_json)
+        print("")
         return response_json
+
+    def write_dict_to_csv(self, audio_feature_dict):
+        with open('tracks_log.csv', 'w', encoding='UTF8', newline='') as f:
+            for key in audio_feature_dict.keys():
+                f.write("%s, %s\n" % (key, audio_feature_dict[key]))
+            # writer = csv.DictWriter(f, fieldnames=fieldnames)
+            # writer.writeheader()
+            # writer.writerows(rows)
+        csv_print = "Writing to tracks_log.csv file ..."
+        return csv_print
 
 
 def main():
@@ -34,6 +50,10 @@ def main():
     token = sfy_cli.get_access_token()
     track_id = "7EmsauPLjAy9XMNELaHBZa?="
     audio_features_json = sfy_cli.get_audio_features(token, track_id)
+    write_to_csv = sfy_cli.write_dict_to_csv(audio_features_json)
+    print(write_to_csv)
+    print("")
+    print("Track's Energy and Valence:")
     print("energy:{}, valence:{}".format(audio_features_json["energy"], audio_features_json["valence"]))
 
     # r = requests.get('https://api.github.com/events')
